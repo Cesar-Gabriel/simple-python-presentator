1214dc5c223348b4966e0aea422fbe2945a89a72
César Gabriel Morín Corona <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-05-03 00:34:23 -0600

    Rename presentator.py to main.py

--- presentator.py
+++
@@ -1,4 +0,0 @@
-name = "John Doe"
-age = 50
-print(f"Hello, my name is", name, "and i am", age, "years old!")
-# You can change this to whatever you want, you can also change John Doe to your real name and age to your real age!
